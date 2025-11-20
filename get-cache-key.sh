be9608886d22d58f313450c624658f0103cfe592
Przemysław Kryger <pkryger@gmail.com>
2025-11-20 12:47:25 +0000

    refactor!: Rename extra-flags to configure-arguments

--- get-cache-key.sh
+++ get-cache-key.sh
@@ -1,7 +1,7 @@
 echo "::group::Get cache key" >&2
 
 if [[ $# -ne 5 ]]; then
-    echo "Usage $0 RUNNER_OS GITHUB_SERVER_URL REPOSITORY BASE_REF FLAGS" >&2
+    echo "Usage $0 RUNNER_OS GITHUB_SERVER_URL REPOSITORY BASE_REF CONFIGURE_ARGUMENTS" >&2
     echo "::engroup::" >&2
     exit 1
 fi
@@ -11,7 +11,7 @@ set -euo pipefail
 runner_os=$1
 repository=$2/$3
 base_ref=$4
-flags=$5
+configure_arguments=$5
 
 repo_sha=$(git ls-remote "${repository}" "${base_ref}" \
           | cut -f 1)
@@ -21,7 +21,7 @@ if [[ -z "${repo_sha}" ]]; then
     exit 2
 fi
 
-sha=$(printf "%s\n%s" "${repo_sha}" "${flags}" \
+sha=$(printf "%s\n%s" "${repo_sha}" "${configure_arguments}" \
           | sort \
           | grep -v -e '^\s*$' \
           | sha256sum \
