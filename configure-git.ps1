c8993ca380779265082fdce98d86220f377f9ea0
Przemysław Kryger <pkryger@gmail.com>
2025-11-22 11:29:50 +0000

    fix: Favour using system git over MSYS2 one
    Emacs's build dependencies declare package git as their dependency.  This
    causes the MSYS2 git located in `<MSYS2-LOCATION>/usr/bin` to take
    precedence over the git installed in the runner, i.e. a regular Windows git.
    
    The MSYS2 git fails when called from Emacs with an argument that contains curly
    braces, e.g.
    
    ``` emacs-lips
    (call-process "git" nil (current-buffer) nil "log" "-1" "@{upstream}")
    ```
    
    This patch adds a symbolic link to the system git in
    `<MSYS2-LOCATION>/usr/local/bin`.  Such a path is added before `/usr/bin` in an
    MSYS2 shell's `$PATH`.

--- configure-git.ps1
+++ configure-git.ps1
@@ -1,5 +1,21 @@
-echo "::group::Run git config"
-git config --global core.autocrlf false
-git config --global core.eol lf
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $MSYS2Location
+)
+
+echo "::group::Configure git line endings"
+git config --global core.autocrlf inherit
 git config -l
+echo "::endgroup::"
+
+echo "::group::Favour using system git"
+$linkDir = Join-Path $MSYS2Location "usr" "local" "bin"
+if (-Not (Test-Path $linkDir)) {
+    New-Item -ItemType Directory -Path $linkDir -Force | Out-Null
+}
+
+$target = "C:\Program Files\Git\bin\git.exe"
+$link   = Join-Path $linkDir "git.exe"
+
+New-Item -ItemType SymbolicLink -Path $link -Target $target
 echo "::endgroup::"
