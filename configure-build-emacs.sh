68706b48d598dbd4775888838b9e93118c5dde75
Przemysław Kryger <pkryger@gmail.com>
2025-12-10 16:08:05 +0000

    refactor: Accept any number of arguments in configure-build-emacs.sh

--- configure-build-emacs.sh
+++ configure-build-emacs.sh
@@ -1,13 +1,29 @@
+#!/usr/bin/env bash
+
 set -euo pipefail
 
-IFS=$'\n' read -r -d '\0' -a args <<< "$1\0"
+re='[^[:alnum:][:blank:]_'\''":/+=-]'
+args=()
+for ((i=1; i<=$#; i++)); do
+    while IFS=$'\n' read -r -a arg; do
+        if [[ "${arg[*]-}" =~ $re ]]; then
+            echo "[$(basename "${0}")] Unsafe argument: ${arg[*]}" >&2
+            exit 1
+        else
+            eval "args+=(${arg[*]-})"
+        fi
+    done < <(printf "%s\n" "${!i}"              \
+                 | grep -v -e '^[[:space:]]*$')
+done
 
 echo "::group::Run autogen.sh"
 ./autogen.sh
 echo "::endgroup::"
 
 echo "::group::Run configure"
-./configure "${args[@]}"
+configure=(./configure "${args[@]}")
+echo "[$(basename "${0}")] Running:" "${configure[@]}"
+"${configure[@]}"
 echo "::endgroup::"
 
 echo "::group::Run make"
