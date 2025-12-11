d7875bc6f1c128f3dab711db10d5b34f3de099dd
Przemysław Kryger <pkryger@gmail.com>
2025-12-11 11:56:52 +0000

    feat: Print name of bash used in a shell wrapper

--- setup-shell-wrapper.sh
+++ setup-shell-wrapper.sh
@@ -3,6 +3,9 @@ echo "::group::Setup shell wrapper"
 dest=${HOME}/.local/bin/setup-emacs-dev-bash
 bash=$(which bash)
 
+echo "[$(basename "${0}")] Using ${bash} for setup-emacs-dev-bash"
+"${bash}" --version
+
 mkdir -p "$(dirname "${dest}")"
 echo "#!${bash}" > "${dest}"
 echo "exec ${bash} --noprofile --norc -e -o pipefail \"\$@\"" >> "${dest}"
