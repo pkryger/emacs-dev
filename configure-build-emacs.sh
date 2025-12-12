50ac6f850403217382ce01c9f75c55a8cf770e54
Przemysław Kryger <pkryger@gmail.com>
2025-12-12 09:12:05 +0000

    fix: Add Homebrew locations to CFLAGS and LDFLAGS on macOS

--- configure-build-emacs.sh
+++ configure-build-emacs.sh
@@ -16,6 +16,33 @@ for ((i=1; i<=$#; i++)); do
                  | grep -v -e '^[[:space:]]*$')
 done
 
+if [[ -n "${HOMEBREW_PREFIX+x}" ]]; then
+    cflag="-isystem=${HOMEBREW_PREFIX}/include"
+    ldflag="-L${HOMEBREW_PREFIX}/lib"
+    for ((i = 0; i < ${#args[@]}; i++)); do
+        elt="${args[${i}]}"
+        if [[ "${elt}" =~ ^CFLAGS= ]]; then
+            if [[ ! "${elt}" =~ ${cflag}$
+                    && ! "${elt}" =~ ${cflag}[[:space:]] ]]; then
+                args[i]="CFLAGS=${cflag} ${elt/#CFLAGS=}"
+            fi
+            cflag=""
+        elif [[ "${elt}" =~ ^LDFLAGS= ]]; then
+            if [[ ! "${elt}" =~ ${ldflag}$
+                    && ! "${elt}" =~ ${ldflag}[[:space:]] ]]; then
+                args[i]="LDFLAGS=${ldflag} ${elt/#LDFLAGS=}"
+            fi
+            ldflag=""
+        fi
+    done
+    if [[ -n "${cflag}" ]]; then
+        args+=("CFLAGS=${cflag}${CFLAGS+ ${CFLAGS}}")
+    fi
+    if [[ -n "${ldflag}" ]]; then
+        args+=("LDFLAGS=${ldflag}${LDFLAGS+ ${LDFLAGS}}")
+    fi
+fi
+
 echo "::group::Run autogen.sh"
 ./autogen.sh
 echo "::endgroup::"
